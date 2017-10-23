d69b245fc02ed11d08ff4045ec671b31d81f006d
Vincent <wendong@ebay.com>
2017-10-23 14:38:48 -0700

    add L14 demo

---
+++ demo/L14-Asteroids-Animation/Astroids/Astroids/AsteroidBehavior.swift
@@ -0,0 +1,110 @@
+//
+//  AsteroidBehavior.swift
+//  Astroids
+//
+//  Created by Dong, Vincent on 10/23/17.
+//  Copyright © 2017 ecg mobile. All rights reserved.
+//
+
+import UIKit
+
+class AsteroidBehavior: UIDynamicBehavior, UICollisionBehaviorDelegate {
+    
+    private lazy var collider: UICollisionBehavior = {
+        let behavior = UICollisionBehavior()
+        behavior.collisionMode = .everything
+        behavior.translatesReferenceBoundsIntoBoundary = true //boundary around reference view
+        behavior.collisionDelegate = self
+        return behavior
+    }()
+    
+    private lazy var physics: UIDynamicItemBehavior = {
+        let behavior = UIDynamicItemBehavior()
+        behavior.elasticity = 1 //how much energy lost in collision; 1 means no loss >1: gain
+        behavior.allowsRotation = true //spinning
+        behavior.friction = 0 //no slowing down
+        behavior.resistance = 0 //resistence to push
+        return behavior
+    }()
+    
+    private var collisionHandlers = [String:()->Void]()
+    
+    func setBoundary(_ path: UIBezierPath?, named name: String, handler: (()->Void)?) {
+        collider.removeBoundary(withIdentifier: name as NSString)
+        collisionHandlers[name] = nil
+        if path != nil {
+            collider.addBoundary(withIdentifier: name as NSString, for: path!)
+            collisionHandlers[name] = handler
+        }
+    }
+    
+    func collisionBehavior(_ behavior: UICollisionBehavior, beganContactFor item: UIDynamicItem, withBoundaryIdentifier identifier: NSCopying?, at p: CGPoint) {
+        if let name = identifier as? String, let handler = collisionHandlers[name] {
+            handler()
+        }
+    }
+    
+    override init() {
+        super.init()
+        addChildBehavior(collider)
+        addChildBehavior(physics)
+    }
+    
+    func addAsteroid(_ asteroid: AsteroidView) {
+        asteroids.append(asteroid)
+        collider.addItem(asteroid)
+        physics.addItem(asteroid)
+    }
+    
+    func removeAsteroid(_ asteroid: AsteroidView) {
+        if let index = asteroids.index(of: asteroid) {
+            asteroids.remove(at: index)
+        }
+        collider.removeItem(asteroid)
+        physics.removeItem(asteroid)
+    }
+    
+    func pushAllAsteroids(by magnitude: Range<CGFloat> = 0..<0.5) {
+        for asteroid in asteroids {
+            let pusher = UIPushBehavior(items: [asteroid], mode: .instantaneous)
+            pusher.magnitude = CGFloat.random(in: magnitude)
+            pusher.angle = CGFloat.random(in: 0..<CGFloat.pi*2)
+            addChildBehavior(pusher)
+        }
+    }
+
+    private var asteroids = [AsteroidView]()
+    
+    // MARK: Recapturing Wayward Asteroids
+    
+    // every 0.5s
+    // we look around for asteroids that are
+    // outside an asteroid's superview
+    // we wrap it around to the other side
+    // we take care to notify the animator that we've moved the item
+    // using updateItem(usingCurrentState:)
+    
+    var recaptureCount = 0
+    private weak var recaptureTimer: Timer?
+    
+    private func startRecapturingWaywardAsteroids() {
+        if recaptureTimer == nil {
+            recaptureTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { [weak self] timer in
+                for asteroid in self?.asteroids ?? [] {
+                    if let asteroidFieldBounds = asteroid.superview?.bounds, !asteroidFieldBounds.contains(asteroid.center) {
+                        asteroid.center.x = asteroid.center.x.truncatingRemainder(dividingBy: asteroidFieldBounds.width)
+                        if asteroid.center.x < 0 { asteroid.center.x += asteroidFieldBounds.width }
+                        asteroid.center.y = asteroid.center.y.truncatingRemainder(dividingBy: asteroidFieldBounds.height)
+                        if asteroid.center.y < 0 { asteroid.center.y += asteroidFieldBounds.height }
+                        self?.dynamicAnimator?.updateItem(usingCurrentState: asteroid)
+                        self?.recaptureCount += 1
+                    }
+                }
+            }
+        }
+    }
+    
+    private func stopRecapturingWaywardAsteroids() {
+        recaptureTimer?.invalidate()
+    }
+}
